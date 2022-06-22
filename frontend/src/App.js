10c499ccd7193594aef8ac98ccd5b7a3177f1fd1
Richard Emmerig <ayasatomichan@gmail.com>
2022-06-22 22:30:35 +0700

    Menambah donaturpage & donaturdetail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,20 @@ import LoginPage from "./pages/LoginPage";
 import BeasiswaPage from "./pages/BeasiswaPage";
 import FormBeasiswa from "./pages/FormBeasiswa";
 import ProfilePage from "./pages/ProfilePage";
-import Navbar from "./components/Navbar/Navbar";
+import DonaturPage from "./pages/DonaturPage";
+import DonaturDetail from "./pages/DonaturDetail";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="login" element={<LoginPage />} />
-            <Route path="beasiswa" element={<BeasiswaPage />} />
-            <Route path="formbeasiswa" component={<FormBeasiswa />} />
-            <Route path="profile" component={<ProfilePage />} />
-          </Route>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/beasiswa' element={< BeasiswaPage />}></Route>
+          <Route exact path='/donatur' element={<DonaturPage />}></Route>
+          <Route exact path='/profile' element={<ProfilePage />}></Route>
+          <Route exact path='/login' element={<LoginPage />}></Route>
+          <Route exact path='/donatur1' element={<DonaturDetail />}></Route>
         </Routes>
       </Router>
     </>
