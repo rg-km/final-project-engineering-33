93af2b702252805b24eb59ad1d4de2da101f3062
Moizac <izfa87aremania@gmail.com>
2022-06-27 22:36:02 +0700

    Update donatur detail, detail beasiswa

--- frontend/src/pages/DonaturDetail.jsx
+++ frontend/src/pages/DonaturDetail.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import Footer from '../components/Footer/Footer'
 import Navbar from '../components/Navbar/Navbar'
 
-function DonaturDetail(){
+function DonaturDetail() {
 
-    return(
+    return (
         <>
-            <Navbar/>
+            <Navbar />
 
             <section class="container mx-auto flex-col px-6 py-4 mt-5 bg-white shadow">
                 <p class="text-xl font-bold text-gray-800">Donatur Profile</p>
@@ -15,38 +15,26 @@ function DonaturDetail(){
 
             <section class="container mx-auto flex flex-wrap py-3">
                 <article class="flex flex-col shadow my-4">
-                    <a href="#" class="mx-auto">
-                        <img class="hover:opacity-75" src="https://images.unsplash.com/photo-1541185933-ef5d8ed016c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+                    <a href="#" class="mx-auto pt-8">
+                        <img class="hover:opacity-75" src="https://images.unsplash.com/photo-1541185933-ef5d8ed016c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
                     </a>
                     <div class="bg-white flex flex-col justify-start p-6">
                         <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">NAMA DONATUR</a>
                         <h1 class="text-2xl font-bold pb-3">Introduction</h1>
                         <p class="pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel neque non libero suscipit suscipit eu eu urna. Proin bibendum urna mattis ante malesuada ultrices. Etiam in turpis vitae elit dictum aliquet. Donec mattis risus in turpis dapibus, eget tempus sem tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In est enim, imperdiet sed ornare quis, pellentesque vel risus. Nunc vitae vestibulum turpis. Quisque eget eleifend urna. Etiam et vulputate purus, ut egestas sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis quis neque non urna venenatis mollis et at massa. Pellentesque sem lacus, malesuada vel hendrerit molestie, mollis vel elit.</p>
-                        <ul className="mt-8 border-2 border-white border-opacity-25 divide-y-2 divide-white sm:grid sm:divide-y-0 sm:divide-x-2 sm:border-l-0 sm:border-r-0 divide-opacity-25 sm:grid-cols-3">
-                            <li className="flex items-center justify-center p-8">
-                                <p className="text-3xl font-extrabold">100+</p>
-                                <p className="mt-1 text-xl font-medium"> Mahasiswa Daftar</p>
-                            </li>
-                            <li className="flex items-center justify-center p-8">
-                                <p className="text-3xl font-extrabold">6</p>
-                                <p className="mt-1 text-xl font-medium"> Bulan</p>
-                            </li>
-                            <li className="flex items-center justify-center p-8">
-                                <p className="text-3xl font-extrabold">8</p>
-                                <p className="mt-1 text-xl font-medium"> Juta</p>
-                            </li>
-                        </ul>
-
-                        <div className='flex items-center justify-end'>
-                            <a className="flex items-center justify-end w-40 px-8 py-3 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring mr-20 mb-3" href="/INI_LARINYA_KE_BEASISWA">
-                                <span className="text-sm font-medium"> DAFTAR </span>
-                                <svg className="w-5 h-5 ml-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                            </a>
-                        </div>
                     </div>
                 </article>
+
+                <div className='flex items-center justify-end'>
+                    <a className="flex items-center justify-end w-40 px-8 py-3 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring mr-20 mb-3">
+                        <Link to='/beasiswa'>
+                            <span className="text-sm font-medium"> DAFTAR </span>
+                        </Link>
+                        <svg className="w-5 h-5 ml-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
+                </div>
 
                 <div class="w-full flex pt-6">
                     <a href="#" class="w-1/2 bg-white shadow hover:shadow-md text-left p-6">
@@ -61,8 +49,8 @@ function DonaturDetail(){
 
             </section>
 
-            <Footer/>
-            
+            <Footer />
+
         </>
     )
 }
